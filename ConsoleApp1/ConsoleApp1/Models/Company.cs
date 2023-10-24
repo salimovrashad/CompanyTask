d69cfd2dc2ca3c48395de997af00e88f0456ec06
salimovrashad <selimovresad33@gmail.com>
2023-10-24 10:28:03 +0400

    add task file

---
+++ ConsoleApp1/ConsoleApp1/Models/Company.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp1.Models
+{
+	internal class Company
+	{
+		private string _name;
+		private Employee[] employee = new Employee[0];
+        public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+		public void AddUser(Employee item)
+		{
+			Array.Resize(ref employee, 1);
+		}
+		public void RemoveUser()
+		{
+
+		}
+		public void GetUser()
+		{
+
+		}
+		public void GetAllUsers()
+		{
+
+		}
+		public void UpdateUser()
+		{
+
+		}
+	}
+}
---
+++ ConsoleApp1/ConsoleApp1/Models/Employee.cs
@@ -0,0 +1,61 @@
+﻿namespace ConsoleApp1.Models
+{
+	internal class Employee
+	{
+		private string _name;
+		private string _surname;
+		private string _username;
+		private int _age;
+
+        public string Name
+		{
+			get{return _name;}
+			set
+			{
+				if (value.Length > 0)
+				{
+					_name = value.ToUpper()[0] + value.Substring(1);
+				}
+				else
+				{
+					Console.WriteLine("Ad duzgun daxil edilmeyib!!!");
+				}
+			}
+		}
+		public string Surname
+		{
+			get { return _surname; }
+			set
+			{
+				if (value.Length > 0)
+				{
+					_surname = value.ToUpper()[0] + value.Substring(1);
+				}
+				else
+				{
+					Console.WriteLine("Soyad duzgun daxil edilmeyib!!!");
+				}
+			}
+		}
+		public string Username
+		{
+			get { return _username; }
+			set { _username = value; }
+		}
+		public int Age
+		{
+			get { return _age; }
+			set 
+			{
+                if (value >= 0)
+                {
+					_age = value;
+				}
+                else
+                {
+                    Console.WriteLine("Yash duzgun daxil edilmeyib!!!");
+                }
+            }
+		}
+	}
+}
